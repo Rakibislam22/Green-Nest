425a99415fdd75cae0c7b3d8231e46ec0a9d04c6
Rakibislam22 <rakibislam13122018@gmail.com>
2025-10-22 00:12:31 +0600

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+
+const Router = createBrowserRouter([
+    {path: "/", Component:MainLayout},
+
+])
+
+export default Router;
