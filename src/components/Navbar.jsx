e1258135450b22d1dfc34ac780e134a27db5f034
Rakibislam22 <rakibislam13122018@gmail.com>
2025-10-22 20:16:50 +0600

    Add NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
+    const links = <>
+        <NavLink className="text-secondary font-bold">Home</NavLink>
+        <NavLink className="lg:pl-6 text-secondary font-bold">Plants</NavLink>
+        <NavLink className="lg:pl-6 text-secondary font-bold">My Profile</NavLink>
+    </>
     return (
-        <div>
-            <h1>I am Nav</h1>
+        <div className="bg-base-100 shadow-sm">
+            <div className='navbar lg:w-11/12 mx-auto '>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
+                    </div>
+                    <Link className="font-bold text-2xl text-primary">GreenNest</Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end space-x-2">
+                    <a className="px-7 btn border-primary hover:bg-primary hover:text-white">Login</a>
+                    <a className="px-7 btn btn-primary">Register</a>
+                </div>
+            </div>
         </div>
     );
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className=' quicksand-font'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
